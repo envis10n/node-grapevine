6e3601a94f96cec1d7496d630cbd58dfa2b02875
envis10n <envis10n@protonmail.com>
2019-02-14 04:54:59 -0600

    Added helper methods

--- src/grapevine.js
+++ src/grapevine.js
@@ -1,4 +1,5 @@
 const backend = require('./backend');
+const uuid = require('uuid/v4');
 const EventEmitter = require('events');
 
 /**
@@ -14,6 +15,7 @@ class Grapevine extends EventEmitter {
      * @param {Array} options.supports Supported features of grapevine.
      * @param {Array} options.channels Channels to subscribe to on authentication.
      * @param {String} options.user_agent User agent provided to grapevine for your game.
+     * @param {Array} options.players List of players currently online. Useful if you have players on already when creating.
      */
     constructor(client_id, client_secret, options){
         super();
@@ -23,7 +25,7 @@ class Grapevine extends EventEmitter {
         this.client_id = client_id;
         this.client_secret = client_secret;
         this.supports = this.options.supports || [];
-        this.clients = new Map();
+        this.players = this.options.players || [];
         this.channels = this.options.channels || [];
         this.user_agent = this.options.user_agent || `NodeGrapevine ${PACKAGE_VERSION}`;
         this.socket = null;
@@ -56,11 +58,44 @@ class Grapevine extends EventEmitter {
                 }
             });
             await this.wait('authenticate');
+            this.socket.on('grapevine/heartbeat', () => {
+                this.socket.send({
+                    event: "heartbeat",
+                    payload: {
+                        players: this.players
+                    }
+                });
+                this.emit('heartbeat');
+            });
             this.emit('authenticated');
         })();
     }
     /**
-     * 
+     * Send a sign-in player event.
+     * @param {String} name Name of the player to send a sign-in event for.
+     */
+    sign_in(name){
+        this.socket.send({
+            event: 'players/sign-in',
+            payload: {
+                name
+            }
+        });
+    }
+    /**
+     * Send a sign-out player event.
+     * @param {String} name Name of the player to send a sign-out event for.
+     */
+    sign_out(name){
+        this.socket.send({
+            event: 'players/sign-out',
+            payload: {
+                name
+            }
+        });
+    }
+    /**
+     * Wait for a grapevine event.
      * @param {String} event Name of the event to wait on.
      * @return {Promise} A promise that resolves to the object sent from grapevine.
      */
@@ -69,6 +104,96 @@ class Grapevine extends EventEmitter {
             let events = this.event_listeners.get(event) || [];
             events.push({resolve, reject});
             this.event_listeners.set(event, events);
+        });
+    }
+    /**
+     * Send a status event. Causes grapevine to send players/status for each game online, unless a game name is provided.
+     * @param {String} game (Optional) Get the status of a specific game.
+     */
+    get_status(game){
+        if(typeof game == "string" && game.length > 0) {
+            this.socket.send({
+                event: 'games/status',
+                ref: uuid(),
+                payload: {
+                    game
+                }
+            });
+        } else {
+            this.socket.send({
+                event: 'players/status',
+                ref: uuid()
+            });
+        }
+    }
+    /**
+     * Add a player to the internal player list used for the online players status.
+     * @param {String} name Name of the player to add to the list.
+     * @param {Boolean} online Whether or not to send a sign in event to grapevine.
+     * @return {Boolean} Whether or not the operation was successful.
+     */
+    add_player(name, online){
+        if(!this.players.find(el=>el == name)){
+            this.players.push(name);
+            if(online){
+                this.sign_in(name);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * Remove a player from the internal player list used for the online players status.
+     * @param {String} name Name of the player to remove from the list.
+     * @param {Boolean} online Whether or not to send a sign out event to grapevine.
+     * @return {Boolean} Whether or not the operation was successful.
+     */
+    remove_player(name, online){
+        let i = this.players.findIndex(el=>el == name);
+        if(i != -1){
+            this.players.splice(i, 1);
+            if(online){
+                this.sign_out(name);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * Send a tell to another player via grapevine.
+     * @param {String} message Message to be sent.
+     * @param {String} from Name of the player sending the tell.
+     * @param {String} target Name of the player the tell is being sent to. May be in the format Player@Game instead of explicitly providing the game name.
+     * @param {String} game_name Name of the game the player is playing.
+     * @return {Promise} A promise the resolves to sent, or rejects with a send error.
+     */
+    send_tell(message, from, target, game_name){
+        return new Promise(async (resolve, reject) => {
+            let spl = target.split('@');
+            let game = game_name;
+            if(spl.length > 1){
+                game = spl[1];
+            }
+            target = spl[0];
+            try {
+                this.socket.send({
+                    event: 'tells/send',
+                    ref: uuid(),
+                    payload: {
+                        from_name: from,
+                        to_game: game,
+                        to_name: target,
+                        sent_at: new Date().toISOString(),
+                        message
+                    }
+                });
+                let dobj = await this.wait('tells/send');
+                resolve('Sent');
+            } catch(e) {
+                reject(e);
+            }
         });
     }
 }
